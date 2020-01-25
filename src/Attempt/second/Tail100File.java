a0aa9fcb18f675850bcbe4f26426ba304943cce7
Dharmesh 💤 <sdharms@amazon.com>
2020-01-26 04:39:07 +0530

    [Attempt 2] Contains the code related to the attempt 2 of printing the last 100 line of a file

--- src/Attempt/second/Tail100File.java
+++ src/Attempt/second/Tail100File.java
@@ -1,4 +1,4 @@
-package Attempt.2;
+package Attempt.second;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -7,12 +7,12 @@ import java.io.RandomAccessFile;
 
 /**
  * Attempt_2
- * Performance - java Tail100File  1.63s user 1.82s system 102% cpu 3.357 total
+ * Performance - java Attempt.second.Tail100File  1.69s user 1.86s system 101% cpu 3.479 total
  * Actual - tail -100 bigFile.txt  0.00s user 0.00s system 78% cpu 0.007 total
  */
 public class Tail100File {
     public static void main(String[] args) throws IOException {
-        final RandomAccessFile randomAccessFile = new RandomAccessFile("../bigFile.txt", "r");
+        final RandomAccessFile randomAccessFile = new RandomAccessFile("./bigFile.txt", "r");
         final BufferedReader bufferedReader = new BufferedReader(new FileReader("bigFile.txt"));
         final long totalLines = bufferedReader.lines().count();
         final long printStartingLineNumber = totalLines - 100;
